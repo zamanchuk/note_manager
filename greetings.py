faa2af057a429d433ab78bb2b25fc57fc44b6671
zamanchuk <zamanchuk@gmail.com>
2025-01-15 10:15:11 +0300

    Add files via upload

---
+++ greetings.py
@@ -0,0 +1,15 @@
+from tkinter.constants import ACTIVE
+
+username = "Рустам"
+title = "Домашка"
+content = "Одна из домашек"
+status = ACTIVE
+created_date = "15.01.2024"
+issue_date = "22.01.2024"
+
+print("Имя пользователя: ", username)
+print("Заголовок заметки: ", title)
+print("Описание заметки: ", content)
+print("Статус заметки: ", status)
+print("Дата создания заметки: ", created_date)
+print("Дата истечения заметки: ", issue_date)
